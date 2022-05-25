30b3dff60d02c43b7fb6a32ecbde6f9f110c35ff
Orion247 <dallinmolson@gmail.com>
2022-05-25 03:23:01 -0600

    a

--- Classes/Deck.cs
+++ Classes/Deck.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 
 namespace CSE210_02.Classes
 {
-
-
     public class Deck
     {
         public List<string> CreateDeck()
@@ -16,28 +14,41 @@ namespace CSE210_02.Classes
             suite.Add("Club");
 
             List<string> deckLiteral = new List<string>();
-            List<int> deckNumerical = new List<int>();
-            for (int x = 1; x <= 4; x++)
+
+            for (int x = 0; x < 4; x++)
             {
-                for (int y = 1; y <= 10; y++)
+                for (int y = 2; y <= 10; y++)
                 {
-                    string line = y + " " + suite[x];
-                    deckLiteral.Add(line);
+                    string cardNumber = y.ToString();
+                    //string line = y.ToString() + " " + suite[x];
+                    deckLiteral.Add(y.ToString() + " " + suite[x]);
+                    //Console.WriteLine(deckLiteral[y - 2]);
                 }
                 deckLiteral.Add("J " + suite[x]);
                 deckLiteral.Add("Q " + suite[x]);
                 deckLiteral.Add("K " + suite[x]);
                 deckLiteral.Add("A " + suite[x]);
 
-                int zeda = 1;
-                for (zeda = 1; zeda <= 13; zeda++);
-                {
-                    deckNumerical.Add(zeda);
-                }
-
             }
 
             return (deckLiteral);
+        }
+
+        public List<int> CreateHierarchy()
+        {
+            List<int> deckNumerical = new List<int>();
+            int z = 0;
+            int v = 0;
+            for (v = 0; v < 4; v++)
+            {
+                for (z = 1; z < 14; z++)
+                {
+                    deckNumerical.Add(z);
+                }
+            }
+
+            return(deckNumerical);
+
         }
     }
 }
--- Classes/Game.cs
+++ Classes/Game.cs
@@ -9,14 +9,84 @@ namespace CSE210_02.Classes
         public void StartGame()
         {
             Deck newDeck = new Deck();
-            List<string> test = newDeck.CreateDeck();
-            int x = 0;
-            for (x = 0; x <= 52; x++) ;
+            List<string> deck = newDeck.CreateDeck();
+            List<int> location = newDeck.CreateHierarchy();
+            int points = 300;
+
+            for (int a = 0; a < 52; a++)
             {
-                Console.WriteLine(test[x]);
+                Console.WriteLine(deck[a]);
+                Console.WriteLine(location[a]);
             }
 
-        }
-    }
+            Random rnd = new Random();
 
+            while (points > 0)
+            {
+                int cardPosition = rnd.Next(deck.Count); 
+                string startingCard = deck[cardPosition];
+                int startingCardValue = location[cardPosition];
+
+                Console.WriteLine($"The Card is: {startingCard}");
+                deck.Remove(deck[cardPosition]);
+                location.Remove(location[cardPosition]);
+
+                cardPosition = rnd.Next(deck.Count);
+                string nextCard = deck[cardPosition];
+                int nextCardValue = location[cardPosition];
+
+                Console.Write("Higher or Lower (H/L)? ");
+                string guess = Console.ReadLine();
+
+                Console.WriteLine($"The next card is: {nextCard}");
+                deck.Remove(deck[cardPosition]);
+                location.Remove(location[cardPosition]);
+
+                Console.WriteLine(startingCardValue);
+                Console.WriteLine(nextCardValue);
+
+                if (startingCardValue < nextCardValue)
+                {
+                    if (guess == "H")
+                    {
+                        points = points + 100;
+                    }
+                    else
+                    {
+                        points = points - 75;
+                    }
+                }
+                if (startingCardValue > nextCardValue)
+                {
+                    if (guess == "L")
+                    {
+                        points = points + 100;
+                    }
+                    else
+                    {
+                        points = points - 75;
+                    }
+                }
+
+                Console.WriteLine($"Your score is {points}");
+                Console.Write("Play again (Y/N)? ");
+                string repeat = Console.ReadLine();
+
+                if (repeat == "N")
+                {
+                    Environment.Exit(0);
+                }
+                if (points == 0)
+                {
+                    Console.WriteLine("Game Over");
+                    Environment.Exit(0);
+                }
+                if (deck.Count == 0)
+                {
+                    Console.WriteLine("Game Over");
+                    Environment.Exit(0);
+                }
+            }
+        }
+   }
 }
