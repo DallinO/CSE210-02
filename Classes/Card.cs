6c7edbc4ee86f965d92cc8369617634993c3293e
DallinO <86103274+DallinO@users.noreply.github.com>
2022-07-15 19:08:48 -0600

    Update Card.cs

--- Classes/Card.cs
+++ Classes/Card.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace CSE210_02.Classes
 {
+    // Creates a new Card object.
+    // Primary function is to return a random card from the
+    // deck object.
     public class Card
     {
         int index;
