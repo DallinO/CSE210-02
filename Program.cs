5aecac013311adbfa5f0a24edeec9f24ab2fbeff
DallinO <86103274+DallinO@users.noreply.github.com>
2022-07-15 19:07:05 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿using CSE210_02.Classes;
+// Hilo
+// Dallin Olson
+// CSE210-02
+
+
+using CSE210_02.Classes;
 
 namespace CSE210_02
 {
+    // Primary program class.
     public class Program
     {
+        // Main method that starts the game.
         static void Main(string[] args)
         {
             Console.Write("Do you want to start a New Game? ");
